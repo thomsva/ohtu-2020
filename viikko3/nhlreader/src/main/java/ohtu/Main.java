bd385eda3fb49f27d1876b2719a08c8b1129226a
Thomas <thomsva@gmail.com>
2020-11-12 14:22:08 +0200

    formatted player list

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -2,30 +2,42 @@ package ohtu;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.Arrays;
 import org.apache.http.client.fluent.Request;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
-        
+
         String bodyText = Request.Get(url).execute().returnContent().asString();
-                
+
         //System.out.println("json-muotoinen data:");
         //System.out.println( bodyText );
-
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
-        
+
         System.out.println("Players from FIN " + java.util.Calendar.getInstance().getTime());
         System.out.println("");
         
+        Arrays.sort(players);
+
         for (Player player : players) {
-            
+
             if (player.getNationality().equals("FIN")) {
                 // Suomi mainittu!
-                System.out.println(player);
+                
+                System.out.printf("%-22s", player.getName());
+                System.out.printf("%-5s", player.getTeam());
+                System.out.printf("%2s", player.getGoals());
+                System.out.print(" + ");
+                System.out.printf("%2s", player.getAssists());
+                System.out.print(" = ");
+                System.out.printf("%2s", player.getTotal());
+                System.out.print("\n");
+                
             }
-        }   
+        }
     }
-  
+
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,15 +1,40 @@
-
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player>{
+
     private String name;
     private String nationality;
     private int assists;
     private int goals;
-    private int penalties; 
+    private int penalties;
     private String team;
     private int games;
-    
+
+
+
+    public int getTotal() {
+        return assists + goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public int getGames() {
+        return games;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -18,14 +43,19 @@ public class Player {
     public String getName() {
         return name;
     }
-    
+
     public String getNationality() {
         return nationality;
     }
 
     @Override
     public String toString() {
-        return name + " team: " + team + " goals:" + goals + " assists:"+ assists;
+        return name + " team: " + team + " goals:" + goals + " assists:" + assists;
     }
-      
+
+    @Override
+    public int compareTo(Player p) {
+        return (p.getTotal()-this.getTotal());
+    }
+
 }
