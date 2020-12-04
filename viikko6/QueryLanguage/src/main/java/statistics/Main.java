89e529a38e12995c686a2c362d9471c5aa9b999f
Thomas <thomsva@gmail.com>
2020-12-04 17:24:43 +0200

    Created Or class.

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -18,11 +18,23 @@ public class Main {
 //                new Not(new HasAtLeast(1, "goals")),
 //                new PlaysIn("NYR")
 //        );
+//
+//        Matcher m = new And(
+//                new HasFewerThan(1, "goals"),
+//                new PlaysIn("NYR")
+//        );
+//        Matcher m = new Or(new HasAtLeast(40, "goals"),
+//                new HasAtLeast(60, "assists")
+//        );
 
-Matcher m = new And( 
-    new HasFewerThan(1, "goals"), 
-    new PlaysIn("NYR")
-);
+        Matcher m = new And(
+            new HasAtLeast(50, "points"),
+            new Or(
+                new PlaysIn("NYR"),
+                new PlaysIn("NYI"),
+                new PlaysIn("BOS")
+            )
+        );
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
